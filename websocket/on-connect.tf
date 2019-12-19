65abce5fcee6163c0890eec738e902575152c37a
Charles Boadu <ekowcharles@gmail.com>
2019-12-19 00:05:06 -0600

    Rename uploaded zip file to reflect function name

--- websocket/on-connect.tf
+++ websocket/on-connect.tf
@@ -19,12 +19,12 @@ EOF
 }
 
 resource "aws_lambda_function" "on_connect" {
-  filename      = "./on-connect/target/bin/main.zip"
+  filename      = "./on-connect/target/bin/on-connect.zip"
   function_name = "on-connect"
   role          = "${aws_iam_role.allow_on_connect.arn}"
   handler       = "main"
 
-  source_code_hash = "${filebase64sha256("./on-connect/target/bin/main.zip")}"
+  source_code_hash = "${filebase64sha256("./on-connect/target/bin/on-connect.zip")}"
 
   runtime = "go1.x"
 
