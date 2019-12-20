c2eb298a6f17397a9e325d27f62185b43391423a
Charles Boadu <ekowcharles@gmail.com>
2019-12-19 22:32:57 -0600

    Allow onConnect function to insert documents into chats table

--- websocket/functions.tf
+++ websocket/functions.tf
@@ -18,6 +18,26 @@ resource "aws_iam_role" "allow_on_connect" {
 EOF
 }
 
+resource "aws_iam_role_policy" "allow_on_connect_to_dynamo_db" {
+  name = "allow-on-connect-to-dynamo-db"
+  role = "${aws_iam_role.allow_on_connect.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:BatchWriteItem"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:dynamodb:us-east-1:*:table/flyingchat"
+    }
+  ]
+}
+EOF
+}
+
 resource "aws_lambda_function" "on_connect" {
   filename      = "./on-connect/target/bin/on-connect.zip"
   function_name = "onConnect"
