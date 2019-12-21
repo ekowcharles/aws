b54d21e54710e3d6ad4ebb40f1d013b561024435
Charles Boadu <ekowcharles@gmail.com>
2019-12-20 23:38:10 -0600

    Change dynamo db key

--- websocket/on-connect/src/main.go
+++ websocket/on-connect/src/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 )
 
 // Request represents the requested object
 type Request struct {
-	ID    int    `json:"ID"`
-	Value string `json:"Value"`
+	ConnectionID string `json:"connection_id"`
 }
 
 // Response represents the Response object
@@ -20,8 +23,50 @@ type Response struct {
 
 // Handler represents the Handler of lambda
 func Handler(request Request) (Response, error) {
+	ddb := dynamodb.New(session.New())
+	input := &dynamodb.PutItemInput{
+		Item: map[string]*dynamodb.AttributeValue{
+			"ConnectionId": {
+				S: aws.String(request.ConnectionID),
+			},
+		},
+		TableName: aws.String("flying-chat"),
+	}
+
+	msg := ""
+	_, err := ddb.PutItem(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case dynamodb.ErrCodeConditionalCheckFailedException:
+				msg = fmt.Sprint(dynamodb.ErrCodeConditionalCheckFailedException, aerr.Error())
+			case dynamodb.ErrCodeProvisionedThroughputExceededException:
+				msg = fmt.Sprint(dynamodb.ErrCodeProvisionedThroughputExceededException, aerr.Error())
+			case dynamodb.ErrCodeResourceNotFoundException:
+				msg = fmt.Sprint(dynamodb.ErrCodeResourceNotFoundException, aerr.Error())
+			case dynamodb.ErrCodeItemCollectionSizeLimitExceededException:
+				msg = fmt.Sprint(dynamodb.ErrCodeItemCollectionSizeLimitExceededException, aerr.Error())
+			case dynamodb.ErrCodeTransactionConflictException:
+				msg = fmt.Sprint(dynamodb.ErrCodeTransactionConflictException, aerr.Error())
+			case dynamodb.ErrCodeRequestLimitExceeded:
+				msg = fmt.Sprint(dynamodb.ErrCodeRequestLimitExceeded, aerr.Error())
+			case dynamodb.ErrCodeInternalServerError:
+				msg = fmt.Sprint(dynamodb.ErrCodeInternalServerError, aerr.Error())
+			default:
+				msg = fmt.Sprint(aerr.Error())
+			}
+		} else {
+			msg = fmt.Sprint(err.Error())
+		}
+
+		return Response{
+			Message: fmt.Sprintf("Process Connection Id %s. Error: %s", request.ConnectionID, msg),
+			Ok:      false,
+		}, err
+	}
+
 	return Response{
-		Message: fmt.Sprint("Process Request Id %f", request.ID),
+		Message: fmt.Sprintf("Process Connection Id %s", request.ConnectionID),
 		Ok:      true,
 	}, nil
 }
--- websocket/persistence.tf
+++ websocket/persistence.tf
@@ -2,10 +2,10 @@ resource "aws_dynamodb_table" "chat" {
   name           = "flying-chat"
   billing_mode   = "PAY_PER_REQUEST"
 
-  hash_key       = "RequestID"
+  hash_key       = "ConnectionId"
 
   attribute {
-    name = "RequestID"
+    name = "ConnectionId"
     type = "S"
   }
 }
