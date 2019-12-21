fd9f44e87fbce9bc42cd9376c5afd9e95ae9f63d
Charles Boadu <ekowcharles@gmail.com>
2019-12-20 23:41:51 -0600

    Pass dynamo db permissions per function

--- websocket/functions.tf
+++ websocket/functions.tf
@@ -1,13 +1,27 @@
+locals {
+  dynamo_db_arn = aws_dynamodb_table.chat.arn
+}
+
 module "on-connect" {
   source = "./modules/functions"
 
   name = "on-connect"
-  dynamodb_arn = aws_dynamodb_table.chat.arn
+  dynamodb_arn = local.dynamo_db_arn
+  dynamo_db_permissions = ["dynamodb:PutItem"]
+}
+
+module "on-disconnect" {
+  source = "./modules/functions"
+
+  name = "on-disconnect"
+  dynamodb_arn = local.dynamo_db_arn
+  dynamo_db_permissions = ["dynamodb:DeleteItem"]
 }
 
 module "on-send" {
   source = "./modules/functions"
 
   name = "on-send"
-  dynamodb_arn = aws_dynamodb_table.chat.arn
+  dynamodb_arn = local.dynamo_db_arn
+  dynamo_db_permissions = ["dynamodb:Scan"]
 }
